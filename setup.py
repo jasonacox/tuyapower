391ecc00d294b53fd55e8962c141b9233108ae3d
Jason Cox <jasonacox@me.com>
2019-10-12 18:20:29 -0700

    Updated package for pypi release

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("tuyapower/README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="tuyapower-jasonacox",
-    version="0.0.1",
+    name="tuyapower",
+    version="0.0.4",
     author="Jason Cox",
     author_email="jason@jasonacox.com",
     description="Pull power and state data from Tuya WiFi smart devices",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="ttps://github.com/jasonacox/tuyapower",
+    url="https://github.com/jasonacox/tuyapower",
     packages=setuptools.find_packages(),
     classifiers=[
+        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
 )
